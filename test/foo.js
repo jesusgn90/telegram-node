421fec4e42e58c2a30f24366d21cf1e7f3e4f341
Jesús Ángel <jesusgonzaleznovez@gmail.com>
2016-10-19 20:15:08 +0200

    Create foo.js

---
+++ test/foo.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Example test
+ */
+describe('Foo', function() {
+    
+        it('Example test', function(done) {
+            console.info('Please code a test here!');
+            done();
+        });
+});
