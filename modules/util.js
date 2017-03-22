348286423bff654eecd3d6fa2b324a1d9d5a9646
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-03-22 21:47:10 +0100

    More ES6

--- modules/util.js
+++ modules/util.js
@@ -1,12 +1,13 @@
 /* jshint esversion: 6 */
 'use strict';
-(function () {
+{
     const foo = () => {
         console.log('Foo function');
         return null;
     };
 
     module.exports = {
-        foo: foo
+        foo
     };
-})();
+
+}
